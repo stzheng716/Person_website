62d3c6fbfd805e4f04d870eed9fff5971a5846d4
Steven <stzheng716@gmail.com>
2023-06-23 22:26:54 -0700

    add navbar logic

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/*===============stick ===============*/
+window.onscroll = () => {
+    let header = document.querySelector('.header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
