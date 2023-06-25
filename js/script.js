b6421bbd63a74923f88166283fb723b509d90dc3
Steven <stzheng716@gmail.com>
2023-06-24 22:35:02 -0700

    add testimonial section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-/*===============stick ===============*/
+/*===============sticky navbar ===============*/
 window.onscroll = () => {
     let header = document.querySelector('.header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
 };
+
+/*===============swiper===============*/
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
